dc3fdc9667597799e54a89c55643fdcec0b5d638
didapax <alfonsi.acosta@gmail.com>
2024-06-20 09:24:45 -0400

    [TEST] PRUEBA GIT DANIEL

--- index.php
+++ index.php
@@ -11,11 +11,13 @@ $tmodulo->crear();
   <head>
     <?php    
     //define('ROOT_PATH1', $_SERVER['DOCUMENT_ROOT'] . '/ProyectoPanaderia');
+        //prueba GIT DANIEL');
+
     header("Location:Vista/index.php");
      ?>
     <title>Inicio Sesion</title>
     <meta name="viewport" content="width=device_width, initial-scale=1.0">
-
+      
     <body>
 
     </body>
