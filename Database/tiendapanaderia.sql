a55f72a566ba572850a51ea051cc4b74017c3c8b
didapax <alfonsi.acosta@gmail.com>
2024-08-18 22:15:43 -0400

    base de adatos Actualizacion

--- Database/tiendapanaderia.sql
+++ Database/tiendapanaderia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2024 a las 23:06:37
+-- Tiempo de generación: 19-08-2024 a las 04:14:25
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
